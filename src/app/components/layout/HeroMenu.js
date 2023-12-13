c83fbe49ca9dd4351a1cebeb96f6ac43b8a60456
Jay Oh <jungwon.oh0920@gmail.com>
2023-12-13 10:27:55 -0800

    menu item layout with a btn.

--- src/app/components/layout/HeroMenu.js
+++ src/app/components/layout/HeroMenu.js
@@ -5,19 +5,19 @@ import MenuItem from './menu/MenuItem'
 const HeroMenu = () => {
     return (
         <section>
-            <div className='relative bg-primary border-slate-4'>
+            {/* <div className='relative bg-primary border-slate-4'>
                 <div className='absolute left-0 w-36 h-80 overflow-hidden'>
                     <Image src='/images/spoon.png' fill className='object-cover -ml-12' />
                 </div>
                 <div className='absolute right-0 w-36 h-80 overflow-hidden'>
                     <Image src='/images/chopsticks.png' fill className='object-cover ml-12' />
                 </div>
-            </div>
+            </div> */}
             <div className='text-center'>
                 <h3 className='font-semibold text-gray-400 leading-4'>Check out</h3>
-                <h3 className='text-primary font-bold text-4xl italic'>Menu</h3>
+                <h2 className='text-primary font-bold text-4xl italic'>Menu</h2>
             </div>
-            <div className='grid grid-cols-3 gap-4'>
+            <div className='grid grid-cols-3 gap-4 mt-4'>
                 <MenuItem />
                 <MenuItem />
                 <MenuItem />
--- src/app/components/layout/menu/MenuItem.js
+++ src/app/components/layout/menu/MenuItem.js
@@ -3,12 +3,16 @@ import Image from 'next/image'
 
 const MenuItem = () => {
     return (
-        <div className='bg-gray-200 w-48 overflow-hidden text-center mx-auto'>
+        <div className='bg-gray-200 overflow-hidden text-center mx-auto px-4 rounded-lg hover:bg-white hover:shadow-2xl hover:shadow-black/50'>
             <div className='w-full overflow-hidden flex items-center justify-center'>
                 <Image src={'/images/yubu.png'} width={180} height={180} />
             </div>
-            <h3>Yubu</h3>
-        </div>
+            <h3 className='font-bold text-xl my-3'>Yubu</h3>
+            <p p className='text-gray-400 text-sm' > Lorem ipsum dolor, sit amet consectetur adipisicing elit.Consequatur, et ?</p >
+            <button className='bg-primary text-white px-4 py-1 rounded-full flex items-center mx-auto my-3'>
+                Add to Cart $5
+            </button>
+        </div >
     )
 }
 
